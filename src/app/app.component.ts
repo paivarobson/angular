2fa731d2025fde81cfc6f6c793a972e53bbebb81
Robson Paiva <paivawork@hotmail.com>
2024-09-26 17:26:52 -0300

    Adicionado componente contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     ContainerComponent,
     CabecalhoComponent,
     SeparadorComponent,
+    ContatoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',

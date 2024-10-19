496d6ec2aa28f6dd63b0a69f2b87c5d864225de9
Robson Paiva <paivawork@hotmail.com>
2024-10-19 12:04:27 -0300

    Ajustando comportamento do botão Voltar da página Perfil Contato

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato/contato';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
-  imports: [CommonModule, ContainerComponent],
+  imports: [CommonModule, ContainerComponent, RouterLink],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css',
 })
